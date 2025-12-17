a5c8f0be8b5a6a750c30c8829d33198eb9646d8c
NoinBlake336 <josechocobar336@gmail.com>
2025-12-17 00:00:59 -0300

    17/12/2025

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,6 @@ import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   resolve: {
@@ -15,6 +14,13 @@ export default defineConfig({
     },
   },
   server: {
+    host: true,
+    port: 5173,
     allowedHosts: ["localhost", "bosquesrl.com", "bosquesrl.online"],
+    hmr: {
+      protocol: "wss",
+      host: "bosquesrl.com",
+      port: 443,
+    },
   },
 });
